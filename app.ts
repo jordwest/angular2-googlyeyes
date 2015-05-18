e1a00c8ea66afc8fb1bc09d5bca3f95a255c521c
Jordan West <jordwest@gmail.com>
2015-05-18 16:17:21 +0900

    Tidy up

--- app.ts
+++ app.ts
@@ -9,7 +9,7 @@ import {Eye} from 'eye'
 @View({
   template: `
     <div class="workspace">
-      <img class="base" #image src="http://{{ dataStore.keyword }}.jpg.to" (mousemove)="updateFocalPoint($event)" (click)="addEyes($event)"/>
+      <img class="base" #image src="http://{{ dataStore.keyword }}.jpg.to" (mousemove)="setFocalPoint($event)" (click)="addEye($event)"/>
       <eye *for="#eye of dataStore.eyes" [x]="eye.x" [y]="eye.y" [focusx]="focusX" [focusy]="focusY"></eye>
     </div>
 
@@ -23,23 +23,29 @@ class EyeAppComponent {
   focusX: number = 0;
   focusY: number = 0;
 
+  // Should be using dependency injection here, but couldn't get it to work
+  // ie, constructor(dataStore: DataStore) should have Angular instantiate
+  // a new DataStore instance
   constructor() {
     this.dataStore = new DataStore();
     this.dataStore.deserialize(window.location.hash);
   }
 
-  updateFocalPoint($event: MouseEvent) {
-    this.focusX = $event.offsetX;
-    this.focusY = $event.offsetY;
+  // Update the point at which the eyes are looking
+  setFocalPoint($event: MouseEvent) {
+    this.focusX = $event.layerX;
+    this.focusY = $event.layerY;
   }
 
+  // Set the keyword used for the image search
   setKeyword(keyword:string) {
     this.dataStore.setKeyword(keyword);
     window.location.hash = this.dataStore.serialize();
   }
 
-  addEyes($event: MouseEvent) {
-    this.dataStore.addEye($event.offsetX, $event.offsetY);
+  // Add an eye to the image
+  addEye($event: MouseEvent) {
+    this.dataStore.addEye($event.layerX, $event.layerY);
     window.location.hash = this.dataStore.serialize();
   }
 }
